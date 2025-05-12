c6fec345e7e143ba8952ba24776a3f131eb239a2
m0tja5k <mail.matvei55@gmail.com>
2025-05-12 11:24:15 +0300

    Add files via upload
    обнова11(работает всё кроме завершения игры)

---
+++ обнова11(работает всё кроме завершения игры)/client/WindowManager.cpp
@@ -0,0 +1,75 @@
+#include "WindowManager.h"
+#include "NetworkClient.h"
+
+WindowManager::WindowManager(QObject *parent) : QObject(parent),
+    authWindow(new AuthWindow()),
+    registerWindow(new RegisterWindow()),
+    mainWindow(nullptr),
+    gameWindow(nullptr)
+{
+    connect(authWindow, &AuthWindow::loginRequested, [this](const QString &email, const QString &password) {
+        NetworkClient::instance().loginUser(email, password);
+    });
+
+    connect(authWindow, &AuthWindow::registerRequested, this, &WindowManager::showRegisterWindow);
+
+    connect(registerWindow, &RegisterWindow::registrationRequested,
+            [this](const QString &nick, const QString &email, const QString &pass, const QString &confirm) {
+                NetworkClient::instance().registerUser(nick, email, pass);
+            });
+
+    connect(registerWindow, &RegisterWindow::backToAuthRequested, this, &WindowManager::showAuthWindow);
+    connect(&NetworkClient::instance(), &NetworkClient::registrationSuccess, this, &WindowManager::showAuthWindow);
+    connect(&NetworkClient::instance(), &NetworkClient::loginSuccess, this, &WindowManager::showMainWindow);
+    connect(&NetworkClient::instance(), &NetworkClient::gameReady, this, &WindowManager::showGameWindow);
+}
+
+WindowManager::~WindowManager() {
+    delete authWindow;
+    delete registerWindow;
+    delete mainWindow;
+    delete gameWindow;
+}
+
+void WindowManager::showAuthWindow()
+{
+    if (mainWindow) mainWindow->hide();
+    if (gameWindow) gameWindow->hide();
+    if (registerWindow) registerWindow->hide();
+
+    authWindow->show();
+}
+
+void WindowManager::showRegisterWindow()
+{
+    authWindow->hide();
+    registerWindow->show();
+}
+
+void WindowManager::showMainWindow(const QString &nickname)
+{
+    if (!mainWindow) {
+        mainWindow = new MainWindow();
+        connect(mainWindow, &MainWindow::startGameRequested, [this]() {
+            NetworkClient::instance().requestStartGame();
+        });
+    }
+
+    authWindow->hide();
+    registerWindow->hide();
+    mainWindow->setUserInfo(nickname);
+    mainWindow->show();
+}
+
+void WindowManager::showGameWindow()
+{
+    if (!gameWindow) {
+        gameWindow = new GameWindow();
+        // Подключаем сигналы для игры
+        connect(&NetworkClient::instance(), &NetworkClient::gameStarted, gameWindow, &GameWindow::onGameStarted);
+        connect(&NetworkClient::instance(), &NetworkClient::moveResult, gameWindow, &GameWindow::onMoveResult);
+    }
+
+    if (mainWindow) mainWindow->hide();
+    gameWindow->show();
+}
