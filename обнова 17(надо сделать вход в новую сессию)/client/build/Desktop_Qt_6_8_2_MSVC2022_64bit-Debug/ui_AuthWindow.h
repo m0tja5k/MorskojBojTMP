c99fcc3aba3d083fdb954c7327e32cb3bbfe0a67
Shayrma749506887 <57056275+Shayrma749506887@users.noreply.github.com>
2025-05-14 16:42:31 +0300

    Add files via upload

---
+++ обнова 17(надо сделать вход в новую сессию)/client/build/Desktop_Qt_6_8_2_MSVC2022_64bit-Debug/ui_AuthWindow.h
@@ -0,0 +1,96 @@
+/********************************************************************************
+** Form generated from reading UI file 'AuthWindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_AUTHWINDOW_H
+#define UI_AUTHWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_AuthWindow
+{
+public:
+    QLabel *label;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QLineEdit *nicknameEdit;
+    QLineEdit *passwordEdit;
+    QPushButton *loginButton;
+    QPushButton *registerButton;
+
+    void setupUi(QWidget *AuthWindow)
+    {
+        if (AuthWindow->objectName().isEmpty())
+            AuthWindow->setObjectName("AuthWindow");
+        AuthWindow->resize(437, 498);
+        label = new QLabel(AuthWindow);
+        label->setObjectName("label");
+        label->setGeometry(QRect(170, 30, 101, 20));
+        label->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        verticalLayoutWidget = new QWidget(AuthWindow);
+        verticalLayoutWidget->setObjectName("verticalLayoutWidget");
+        verticalLayoutWidget->setGeometry(QRect(0, 70, 441, 431));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        nicknameEdit = new QLineEdit(verticalLayoutWidget);
+        nicknameEdit->setObjectName("nicknameEdit");
+
+        verticalLayout->addWidget(nicknameEdit);
+
+        passwordEdit = new QLineEdit(verticalLayoutWidget);
+        passwordEdit->setObjectName("passwordEdit");
+        passwordEdit->setEchoMode(QLineEdit::EchoMode::Password);
+        passwordEdit->setReadOnly(false);
+
+        verticalLayout->addWidget(passwordEdit);
+
+        loginButton = new QPushButton(verticalLayoutWidget);
+        loginButton->setObjectName("loginButton");
+
+        verticalLayout->addWidget(loginButton);
+
+        registerButton = new QPushButton(verticalLayoutWidget);
+        registerButton->setObjectName("registerButton");
+
+        verticalLayout->addWidget(registerButton);
+
+
+        retranslateUi(AuthWindow);
+
+        QMetaObject::connectSlotsByName(AuthWindow);
+    } // setupUi
+
+    void retranslateUi(QWidget *AuthWindow)
+    {
+        AuthWindow->setWindowTitle(QCoreApplication::translate("AuthWindow", "Form", nullptr));
+        label->setText(QCoreApplication::translate("AuthWindow", "\320\220\320\262\321\202\320\276\321\200\320\270\320\267\320\260\321\206\320\270\321\217", nullptr));
+        nicknameEdit->setText(QString());
+        nicknameEdit->setPlaceholderText(QCoreApplication::translate("AuthWindow", "nickname", nullptr));
+        passwordEdit->setText(QString());
+        passwordEdit->setPlaceholderText(QCoreApplication::translate("AuthWindow", "password", nullptr));
+        loginButton->setText(QCoreApplication::translate("AuthWindow", "\320\222\320\276\320\271\321\202\320\270", nullptr));
+        registerButton->setText(QCoreApplication::translate("AuthWindow", "\320\240\320\265\320\263\320\270\321\201\321\202\321\200\320\260\321\206\320\270\321\217", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class AuthWindow: public Ui_AuthWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_AUTHWINDOW_H
